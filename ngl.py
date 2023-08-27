b5bddca868169b0146a61fb0ea17cf41163f689f
힝구 <89388901+hinggu9@users.noreply.github.com>
2023-08-27 21:34:15 +0900

    Add files via upload

---
+++ ngl.py
@@ -0,0 +1,18 @@
+import requests, threading, asyncio
+
+instagram = "" #NGL 아이디
+message = "" #질문
+device = "none"
+
+async def go():
+    req = requests.Session()
+    r = req.post(f'https://ngl.link/api/submit', headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'X-Requested-With': 'XMLHttpRequest'}, data={
+                'username': instagram,
+                'question': message,
+                'deviceId': device
+            })
+    print('\033[32m' + f"[ NGL ] Submit 을 Post 시도했어요!" + '\033[0m')
+    print('\033[33m' + f"[ NGL value ] {r}" + '\033[0m')
+
+for i in range(int(100)):
+    threading.Thread(target=asyncio.run, args=(go(),)).start()
